5e9203a61c24395a92a2af553401c15d2f270aee
72-qb <1354081830@qq.com>
2020-08-10 19:55:15 +0800

    "springboot项目的创建和配置文件读取，加入了logback日志系统，2020年8月10号作业上传"

--- java_spring_boot/src/main/java/com/hqyj/javaSpringBoot/modules/test/controller/TestController.java
+++ java_spring_boot/src/main/java/com/hqyj/javaSpringBoot/modules/test/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.hqyj.javaSpringBoot.modules.test.controller;
 
+import com.hqyj.javaSpringBoot.modules.test.vo.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +20,62 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
-    /**
-     *http://localhost:8080/test/test01
-     * get请求
-     *
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+    @Value("${server.port}")
+    private String port;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.age}")
+    private int age;
+    @Value("${com.desc}")
+    private String desc;
+    @Value("${com.random}")
+    private String random;
+
+    @Autowired
+    private Application application;
+
+    /*
+    * http://localhost:8085/test/logTest
+    * */
+    @GetMapping("/logTest")
+    @ResponseBody
+    public String logTest() {
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "This is a log";
+    }
+
+    /*
+     * http://localhost:8085/test/config
      * */
+    @GetMapping("/config")
+    @ResponseBody
+    public String config() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(port).append("********")
+                .append(name).append("********")
+                .append(age).append("********")
+                .append(desc).append("********")
+                .append(random).append("********").append("<br>")
+                .append(application.getPort()).append("********")
+                .append(application.getName()).append("********")
+                .append(application.getAge()).append("********")
+                .append(application.getDesc()).append("********")
+                .append(application.getRandom()).append("********");
+        return sb.toString();
+    }
+
+    /**
+     * http://localhost:8080/test/test01
+     * get请求
+     */
     @GetMapping("/test01")
     @ResponseBody
-    public String test01(){
+    public String test01() {
         return "hello spring-boot";
     }
 }
